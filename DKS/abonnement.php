3e9a6b7b6eeb79878b4f20bb2b2efcc33dee0fab
sophie-syy <susophie16@gmail.com>
2025-10-21 21:15:30 -0300

    Add files via upload

--- DKS/abonnement.php
+++ DKS/abonnement.php
@@ -54,7 +54,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['plan'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Abonnement - Data Keep Safe</title>
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/abonnement.css">
 </head>
 <body>
 <div class="menu">
